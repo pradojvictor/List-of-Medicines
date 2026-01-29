f30b4f23e229d6d0a05faf6cb8148e3430ad1ed7
pradojvictor <joaovictorred@hotmail.com>
2026-01-29 02:00:55 -0300

    logic init

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios';
+import { gifs } from './assets';
+import ToggleBox from './components/Toggle';
+
+function App() {
+  const [medicines, setMedicines] = useState([]);
+  const [hour, setHour] = useState();
+  const [loading, setLoading] = useState(true);
+  const [erro, setErro] = useState(null);
+
+  const [showallMedicines, setShowAllMedicines] = useState(true);
+
+  const medicinesFiltered = showallMedicines
+    ? medicines.filter(med => med.status === 1)
+    : medicines;
+
+  const fetchMedicamentos = async () => {
+    try {
+      setLoading(true);
+      const timer = new Promise((resolve) => setTimeout(resolve, 6000));
+      const apiCall = axios.get('https://api.npoint.io/33b08d2edd649671343a');
+      const [response] = await Promise.all([apiCall, timer]);
+
+      setMedicines(response.data.medicamentos);
+      setHour(response.data.hora);
+    } catch (error) {
+      setErro('Erro ao buscar medicamentos');
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMedicamentos();
+  }, []);
+
+  if (loading) {
+    return <div className="loader">
+      <img src={gifs.img03} alt="Logo" />
+      <h1>carregando.....</h1>
+    </div>;
+  }
+
+  return (
+    <div>
+      
+      <h1>CAPS II LESTE</h1>
+      <ToggleBox isChecked={showallMedicines} onToggle={() => setShowAllMedicines(!showallMedicines)}/>
+      <h3>ultima atualização {hour}</h3>
+      {erro && <p style={{color: 'red'}}>{erro}</p>}
+      <ul>
+        <h1>Medicamentos disponiveis</h1>
+        {medicinesFiltered.map((med, id) => (
+          <div key={id}>
+            <p >{med.name}</p>
+            <p >{med.status}</p>
+          </div>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App;

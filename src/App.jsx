60338cdaa61bab71b981fd8df5a1a55bd2c44641
pradojvictor <joaovictorred@hotmail.com>
2026-03-31 02:58:51 -0300

    fix: button section

--- src/App.jsx
+++ src/App.jsx
@@ -313,7 +313,7 @@ function App() {
             3.923-.627 5.487-1.698l6.238 6.238 2.354-2.354zm-20.955-11.916c0-3.792 3.085-6.877 6.877-6.877s6.877 
             3.085 6.877 6.877-3.085 6.877-6.877 6.877c-3.793 0-6.877-3.085-6.877-6.877z" />
             </svg>
-            <input className='input-search' type='text' placeholder='buscar...' value={search} onChange={e => setSearch(e.target.value)} />
+            <input name="search-medicine" className='input-search' type='text' placeholder='buscar...' value={search} onChange={e => setSearch(e.target.value)} />
           </div>
           {erro && <p style={{ color: 'red' }}>{erro}</p>}
         </div>
@@ -322,7 +322,7 @@ function App() {
             <h3>{showOnlyActive ? "Medicamentos ativos" : "Todos os medicamentos"}</h3>
           </div>
           <section>
-            <label className={`${toggle}`} onClick={handleToggle}>
+            <div role="button" tabIndex="0" onKeyDown={(e) => { if (e.key === 'Enter' || e.key === ' ') { e.preventDefault(); handleToggle();}}} className={`${toggle}`} onClick={handleToggle}>
               <div className={`${toggle}-theme`}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`true-icon`}>
                   <path d="m2.25 12.321 7.27 6.491c.143.127.321.19.499.19.206 0 .41-.084.559-.249l11.23-12.501c.129-.143.192-.321.192-.5 0-.419-.338-.75-.749-.75-.206 0-.411.084-.559.249l-10.731 11.945-6.711-5.994c-.144-.127-.322-.19-.5-.19-.417 0-.75.336-.75.749 0 .206.084.412.25.56" fillRule="nonzero" />
@@ -331,7 +331,7 @@ function App() {
                   <path d="m12 10.93 5.719-5.72c.146-.146.339-.219.531-.219.404 0 .75.324.75.749 0 .193-.073.385-.219.532l-5.72 5.719 5.719 5.719c.147.147.22.339.22.531 0 .427-.349.75-.75.75-.192 0-.385-.073-.531-.219l-5.719-5.719-5.719 5.719c-.146.146-.339.219-.531.219-.401 0-.75-.323-.75-.75 0-.192.073-.384.22-.531l5.719-5.719-5.72-5.719c-.146-.147-.219-.339-.219-.532 0-.425.346-.749.75-.749.192 0 .385.073.531.219z" />
                 </svg>
               </div>
-            </label>
+            </div>
           </section>
         </div>
         <div className='list-container'>

f7b786bf7c781cd0dcf0ec89bfeba27c59377529
pradojvictor <joaovictorred@hotmail.com>
2026-04-06 23:16:34 -0300

    fix: scroll introjs inf-span test

--- src/App.jsx
+++ src/App.jsx
@@ -156,6 +156,13 @@ function App() {
           scrollTo: 'tooltip',
           scrollToElement: false
         },
+         {
+          element: '.div-list-span',
+          intro: 'Dúvidas e suporte, aqui!',
+          position: 'floating',
+          scrollTo: 'tooltip',
+          scrollToElement: false
+        },
       ],
       doneLabel: 'Entendi!',
     }).start();
@@ -335,23 +342,29 @@ function App() {
             </div>
           </section>
         </div>
-        <div className='list-container'>
-          <div className='div-update'>
-            <h3>ultima atualização {hour}</h3>
+        <div className='div-list-span'>
+
+          <div className='list-container'>
+            <div className='div-update'>
+              <h3>ultima atualização {hour}</h3>
+            </div>
+            <div className='div-list' ref={listRef}>
+              {medicinesFiltered.map((med, id) => (
+                <MedicineCard key={id} med={med} />
+              ))}
+            </div>
+            <div>
+            </div>
           </div>
-          <div className='div-list' ref={listRef}>
-            {medicinesFiltered.map((med, id) => (
-              <MedicineCard key={id} med={med} />
-            ))}
-          </div>
-          <div>
-          </div>
-        </div>
+
         <span className={`${isActive}-span-info`} id='span-info' onClick={onMenu}>
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
             <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-.001 5.75c.69 0 1.251.56 1.251 1.25s-.561 1.25-1.251 1.25-1.249-.56-1.249-1.25.559-1.25 1.249-1.25zm2.001 12.25h-4v-1c.484-.179 1-.201 1-.735v-4.467c0-.534-.516-.618-1-.797v-1h3v6.265c0 .535.517.558 1 .735v.999z" />
           </svg>
         </span>
+        </div>
+
+
         <ul className={isActive ? "active" : ""}>
           <div className='div-menu'>
             <FaqMenu Children={handleStartGuide} onClosed={onMenu} classfaq={`${true}-span-info-menu`} emailLink={validarLink(urlFormated(SameEmail))} />
